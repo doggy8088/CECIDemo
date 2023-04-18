9efe06a7d2354a6b432b8cacd07b377848bca818
Will <doggy.huang@gmail.com>
2023-04-18 14:36:50 +0800

    實作「輸入驗證」與「模型驗證」

--- EFCoreDemo/Models/Course.cs
+++ EFCoreDemo/Models/Course.cs
@@ -2,11 +2,12 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace EFCoreDemo.Models
 {
-    public partial class Course
+    public partial class Course : IValidatableObject
     {
         public Course()
         {
@@ -15,8 +16,14 @@ namespace EFCoreDemo.Models
         }
 
         public int CourseId { get; set; }
+
+        [Required(ErrorMessage = "請輸入課程名稱")]
         public string Title { get; set; }
+        
+        [Required(ErrorMessage = "請輸入課程評價")]
+        [Range(1, 5, ErrorMessage = "課程評價請設定 1 ~ 5")]
         public int Credits { get; set; }
+        
         public int DepartmentId { get; set; }
 
         [JsonIgnore]
@@ -26,5 +33,14 @@ namespace EFCoreDemo.Models
 
         [JsonIgnore]
         public virtual ICollection<Person> Instructor { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Title.Contains("CECI") && this.Credits < 3)
+            {
+                yield return new ValidationResult("課程名稱在 Credit 小於 3 的時候不允許出現 CECI 字樣",
+                    new string[] { "Title" });
+            }
+        }
     }
 }
