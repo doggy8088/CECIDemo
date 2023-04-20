5c904493c51a6573f7298ac22b6cde0cb0a8beb4
Will <doggy.huang@gmail.com>
2023-04-20 10:31:24 +0800

    將輸入驗證的參數換成 {1} 變數

--- EFCoreDemo/LoginViewModel.cs
+++ EFCoreDemo/LoginViewModel.cs
@@ -5,11 +5,13 @@ namespace EFCoreDemo
     public class LoginViewModel
     {
         [Required(ErrorMessage = "帳號必填")]
-        [StringLength(16, ErrorMessage = "帳號不能超過 16 個字元")]
+        [StringLength(16, ErrorMessage = "帳號不能超過 {1} 個字元")]
+        [MinLength(3, ErrorMessage = "帳號不可小於 {1} 個字元")]
         public string Username { get; set; } = null!;
 
         [Required(ErrorMessage = "密碼必填")]
-        [StringLength(32, ErrorMessage = "密碼不能超過 32 個字元")]
+        [StringLength(32, ErrorMessage = "密碼不能超過 {1} 個字元")]
+        [MinLength(3, ErrorMessage = "密碼不可小於 {1} 個字元")]
         public string Password { get; set; } = null!;
     }
 }
